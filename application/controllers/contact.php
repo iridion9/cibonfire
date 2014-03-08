e2faf6c746a51885963da18909c396e7c1af5849
nghiaqt <nghiaqt@gmail.com>
2014-03-08 11:56:10 +0700

    first version

---
+++ application/controllers/contact.php
@@ -0,0 +1,49 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Bonfire
+ *
+ * An open source project to allow developers get a jumpstart their development of CodeIgniter applications
+ *
+ * @package   Bonfire
+ * @author    Bonfire Dev Team
+ * @copyright Copyright (c) 2011 - 2013, Bonfire Dev Team
+ * @license   http://guides.cibonfire.com/license.html
+ * @link      http://cibonfire.com
+ * @since     Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Home controller
+ *
+ * The base controller which displays the homepage of the Bonfire site.
+ *
+ * @package    Bonfire
+ * @subpackage Controllers
+ * @category   Controllers
+ * @author     Bonfire Dev Team
+ * @link       http://guides.cibonfire.com/helpers/file_helpers.html
+ *
+ */
+class Contact extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('application');
+		$this->load->library('Template');
+		$this->load->library('Assets');
+		$this->lang->load('application');
+		$this->load->library('events');
+	}
+
+	public function index()
+	{
+		Template::render();
+	}
+}
+?>
